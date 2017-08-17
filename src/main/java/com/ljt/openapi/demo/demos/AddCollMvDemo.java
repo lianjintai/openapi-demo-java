567103a1719cac2ff2768dc1956de0c8dfe7d2f1
zhangzhiqiang <zzq_lqy@163.com>
2017-08-17 20:11:18 +0800

    修改房产、机动车对外接口DEMO

--- src/main/java/com/ljt/openapi/demo/demos/AddCollMvDemo.java
+++ src/main/java/com/ljt/openapi/demo/demos/AddCollMvDemo.java
@@ -58,153 +58,6 @@ public class AddCollMvDemo {
   private String appSecret = "";
 
   /**
-   * @Description : 新增机动车担保品请求参数示例
-   * @return : String
-   * @Creation Date : 2017年8月17日 下午1:57:01
-   * @Author : zhiqiang zhang
-   */
-  public String collMvParams() {
-    AppCollMvApiVO appCollMvApiVO = new AppCollMvApiVO();
-    appCollMvApiVO.setAppId("0092728480d24f5d87bf63639b5cfe1c");
-    appCollMvApiVO.setMtCollStyleCd("DY");
-    appCollMvApiVO.setMtCollCatCd("MV01");
-    appCollMvApiVO.setMtCollCd("DY0601001");
-    appCollMvApiVO.setCollValue(new BigDecimal("123456.78"));
-    appCollMvApiVO.setDtPurchased(new Date());
-    appCollMvApiVO.setMortgageInfo("抵押摘要");
-    appCollMvApiVO.setPurchasedPrc(new BigDecimal("500000.78"));
-
-    MvBasicInfoVO basicInfoVO = new MvBasicInfoVO();
-    basicInfoVO.setDtFirstRecord(new Date());
-    basicInfoVO.setVehicleType("汽车");
-    basicInfoVO.setBrand("奥迪");
-    basicInfoVO.setApprovedMass(new BigDecimal("1000"));
-    basicInfoVO.setApprovedPassenger(6);
-    basicInfoVO.setColor("红");
-    basicInfoVO.setDrivenDistance(new BigDecimal("1000"));
-    basicInfoVO.setEngineCapacity(new BigDecimal("2.0"));
-    basicInfoVO.setIsMadeInChina("N");
-    basicInfoVO.setManufactured(new Date());
-    basicInfoVO.setModel("轿车");
-    List<String> mtFuelCd = new ArrayList<>();
-    mtFuelCd.add("001");
-    mtFuelCd.add("002");
-    mtFuelCd.add("003");
-    basicInfoVO.setMtFuelCd(mtFuelCd);
-    basicInfoVO.setPower(new BigDecimal("232.0"));
-    basicInfoVO.setPriceMax(new BigDecimal("500000.01"));
-    basicInfoVO.setPriceMin(new BigDecimal("20000.01"));
-    basicInfoVO.setSeries("美系车");
-    appCollMvApiVO.setMvBasicInfo(basicInfoVO);
-
-
-    MvConfInfoVO confInfoVO = new MvConfInfoVO();
-    List<String> mtAirconditionTypCd = new ArrayList<>();
-    mtAirconditionTypCd.add("001");
-    mtAirconditionTypCd.add("002");
-    confInfoVO.setMtAirconditionTypCd(mtAirconditionTypCd);
-    List<String> mtCommonConfTypCd = new ArrayList<>();
-    mtCommonConfTypCd.add("001");
-    mtCommonConfTypCd.add("002");
-    confInfoVO.setMtCommonConfTypCd(mtCommonConfTypCd);
-    List<String> mtDriverEleTypCd = new ArrayList<>();
-    mtDriverEleTypCd.add("001");
-    mtDriverEleTypCd.add("002");
-    confInfoVO.setMtDriverEleTypCd(mtDriverEleTypCd);
-    List<String> mtGearboxTypCd = new ArrayList<>();
-    mtGearboxTypCd.add("001");
-    mtGearboxTypCd.add("002");
-    confInfoVO.setMtGearboxTypCd(mtGearboxTypCd);
-    List<String> mtParkingRadarTypCd = new ArrayList<>();
-    mtParkingRadarTypCd.add("001");
-    mtParkingRadarTypCd.add("002");
-    confInfoVO.setMtParkingRadarTypCd(mtParkingRadarTypCd);
-    List<String> mtSeatHeatTypCd = new ArrayList<>();
-    mtSeatHeatTypCd.add("001");
-    mtSeatHeatTypCd.add("002");
-    confInfoVO.setMtSeatHeatTypCd(mtSeatHeatTypCd);
-    appCollMvApiVO.setMvConfInfo(confInfoVO);
-
-
-    MvInsuranceInfo insuranceInfo = new MvInsuranceInfo();
-    insuranceInfo.setDeathPayLimit(new BigDecimal("50000.01"));
-    insuranceInfo.setNoResponDeathPayLimit(new BigDecimal("50000.01"));
-    insuranceInfo.setNoResponMedicalPayLimit(new BigDecimal("50000.01"));
-    insuranceInfo.setNoResponPropertyLossPayLimit(new BigDecimal("50000.01"));
-    insuranceInfo.setPropertyLossPayLimit(new BigDecimal("50000.01"));
-    insuranceInfo.setMedicalPayLimit(new BigDecimal("50000.01"));
-    insuranceInfo.setInsuranceSum(new BigDecimal("50000.01"));
-    insuranceInfo.setRescueFundRate(new BigDecimal("52.22"));
-    insuranceInfo.setRescueFund(new BigDecimal("50000.01"));
-    insuranceInfo.setDtInsuranceStart(new Date());
-    insuranceInfo.setDtInsuranceEnd(new Date());
-    List<String> mtInsuranceResolutionCd = new ArrayList<>();
-    mtInsuranceResolutionCd.add("001");
-    mtInsuranceResolutionCd.add("002");
-    insuranceInfo.setMtInsuranceResolutionCd(mtInsuranceResolutionCd);
-    insuranceInfo.setHasInsurancePayHis("Y");
-    insuranceInfo.setEngineNo("123456789987654321");
-    insuranceInfo.setFrameNo("WFIE231231");
-    insuranceInfo.setPlateNo("JWF1231233");
-    insuranceInfo.setInsurancePaySum(new BigDecimal("50000.01"));
-    insuranceInfo.setAccidentIllegalRate(new BigDecimal("52"));
-    appCollMvApiVO.setMvInsuranceInfo(insuranceInfo);
-
-
-    MvPaymentInfoVO mvPaymentInfoVO = new MvPaymentInfoVO();
-    mvPaymentInfoVO.setCurYearUnpaid(new BigDecimal("200"));
-    mvPaymentInfoVO.setForfeit(new BigDecimal("200"));
-    mvPaymentInfoVO.setLastYearPaid(new BigDecimal("200"));
-    mvPaymentInfoVO.setFeeSum(new BigDecimal("600"));
-    Calendar calendar = Calendar.getInstance();
-    mvPaymentInfoVO.setPaymentYear(new Date(calendar.get(Calendar.YEAR)));
-    appCollMvApiVO.setMvPaymentInfo(mvPaymentInfoVO);
-
-    MvPenaltyInfoVO mvPenaltyInfoVO = new MvPenaltyInfoVO();
-    mvPenaltyInfoVO.setUnclearedPenaltyPoints(5);
-    mvPenaltyInfoVO.setUnpaidPenalty(new BigDecimal("200"));
-    appCollMvApiVO.setMvPenaltyInfo(mvPenaltyInfoVO);
-
-    List<AppCollOwnerVO> csCollOwner = new ArrayList<>();
-    AppCollOwnerVO appCollOwner1 = new AppCollOwnerVO();
-    appCollOwner1.setNm("张三");
-    appCollOwner1.setMtCifIdTypCd("I");
-    appCollOwner1.setIdNo("110113198410126933");
-    appCollOwner1.setMobileNo("13512341234");
-    AppCollOwnerVO appCollOwner2 = new AppCollOwnerVO();
-    appCollOwner2.setNm("李四");
-    appCollOwner2.setMtCifIdTypCd("I");
-    appCollOwner2.setIdNo("110113198410126932");
-    appCollOwner2.setMobileNo("13512341232");
-    csCollOwner.add(appCollOwner1);
-    csCollOwner.add(appCollOwner2);
-    appCollMvApiVO.setCsCollOwner(csCollOwner);
-
-    List<AppCollOwnerVO> cpCollOwner = new ArrayList<>();
-    AppCollOwnerVO appCollOwner3 = new AppCollOwnerVO();
-    appCollOwner3.setNm("北京某某公司");
-    appCollOwner3.setIdNo("110113198410126911");
-    appCollOwner3.setIsComb("Y");
-    AppCollOwnerVO appCollOwner4 = new AppCollOwnerVO();
-    appCollOwner4.setNm("上海某某公司");
-    appCollOwner4.setIdNo("110113198410126922");
-    appCollOwner4.setIsComb("N");
-    cpCollOwner.add(appCollOwner3);
-    cpCollOwner.add(appCollOwner4);
-    appCollMvApiVO.setCsCollOwner(cpCollOwner);
-
-    List<AppCollEvaluateVO> appCollEvaluate = new ArrayList<>();
-    AppCollEvaluateVO appCollEvaluateVO = new AppCollEvaluateVO();
-    appCollEvaluateVO.setEvaluateValue(new BigDecimal("123456.78"));
-    appCollEvaluateVO.setDtEvaluated(new Date());
-    appCollEvaluateVO.setAppraiser("李四");
-    appCollEvaluate.add(appCollEvaluateVO);
-    appCollMvApiVO.setAppCollEvaluate(appCollEvaluate);
-
-    return JSONObject.toJSONString(appCollMvApiVO, SerializerFeature.WriteDateUseDateFormat);
-  }
-
-  /**
    * @Description : 财报上传demo
    * @return : void
    * @Creation Date : 2017年5月8日 下午6:36:08
@@ -237,5 +90,228 @@ public class AddCollMvDemo {
     request.setHeaders(headers);
     request.setStringBody(requestBody);
     Client.execute(request);
+  }
+
+  /**
+   * @Description : 新增机动车担保品请求参数示例
+   * @return : String
+   * @Creation Date : 2017年8月17日 下午1:57:01
+   * @Author : zhiqiang zhang
+   */
+  public String collMvParams() {
+    AppCollMvApiVO appCollMvApiVO = new AppCollMvApiVO();
+    appCollMvApiVO.setAppId("0092728480d24f5d87bf63639b5cfe1c");
+    appCollMvApiVO.setMtCollStyleCd("DY");
+    appCollMvApiVO.setMtCollCatCd("MV01");
+    appCollMvApiVO.setMtCollCd("DY0601001");
+    appCollMvApiVO.setCollValue(new BigDecimal("123456.78"));
+    appCollMvApiVO.setDtPurchased(new Date());
+    appCollMvApiVO.setMortgageInfo("抵押摘要");
+    appCollMvApiVO.setPurchasedPrc(new BigDecimal("500000.78"));
+    // 机动车基本信息
+    appCollMvApiVO.setMvBasicInfo(getMvBasicInfo());
+    // 车辆配置
+    appCollMvApiVO.setMvConfInfo(getMvConfInfo());
+    // 交强险信息
+    appCollMvApiVO.setMvInsuranceInfo(getMvInsuranceInfo());
+    // 代收车船费
+    appCollMvApiVO.setMvPaymentInfo(getMvPaymentInfo());
+    // 违章情况
+    appCollMvApiVO.setMvPenaltyInfo(getMvPenaltyInfo());
+    // 所有者信息（个人）
+    appCollMvApiVO.setCsCollOwner(getCsCollOwner());
+    // 所有者信息（企业）
+    appCollMvApiVO.setCsCollOwner(getCpCollOwner());
+    // 外部评估信息
+    appCollMvApiVO.setAppCollEvaluate(getAppCollEvaluate());
+    return JSONObject.toJSONString(appCollMvApiVO, SerializerFeature.WriteDateUseDateFormat);
+  }
+
+  /**
+   * @Description : 外部评估信息（测试数据）
+   * @return : List<AppCollEvaluateVO>
+   * @Creation Date : 2017年8月17日 下午8:07:48
+   * @Author : zhiqiang zhang
+   */
+  private List<AppCollEvaluateVO> getAppCollEvaluate() {
+    List<AppCollEvaluateVO> appCollEvaluate = new ArrayList<>();
+    AppCollEvaluateVO appCollEvaluateVO = new AppCollEvaluateVO();
+    appCollEvaluateVO.setEvaluateValue(new BigDecimal("123456.78"));
+    appCollEvaluateVO.setDtEvaluated(new Date());
+    appCollEvaluateVO.setAppraiser("李四");
+    appCollEvaluate.add(appCollEvaluateVO);
+    return appCollEvaluate;
+  }
+
+  /**
+   * @Description : 所有者信息（企业）（测试数据）
+   * @return : List<AppCollOwnerVO>
+   * @Creation Date : 2017年8月17日 下午8:07:30
+   * @Author : zhiqiang zhang
+   */
+  private List<AppCollOwnerVO> getCpCollOwner() {
+    List<AppCollOwnerVO> cpCollOwner = new ArrayList<>();
+    AppCollOwnerVO appCollOwner3 = new AppCollOwnerVO();
+    appCollOwner3.setNm("北京某某公司");
+    appCollOwner3.setIdNo("110113198410126911");
+    appCollOwner3.setIsComb("Y");
+    AppCollOwnerVO appCollOwner4 = new AppCollOwnerVO();
+    appCollOwner4.setNm("上海某某公司");
+    appCollOwner4.setIdNo("110113198410126922");
+    appCollOwner4.setIsComb("N");
+    cpCollOwner.add(appCollOwner3);
+    cpCollOwner.add(appCollOwner4);
+    return cpCollOwner;
+  }
+
+  /**
+   * @Description : 所有者信息（个人）（测试数据）
+   * @return : List<AppCollOwnerVO>
+   * @Creation Date : 2017年8月17日 下午8:07:16
+   * @Author : zhiqiang zhang
+   */
+  private List<AppCollOwnerVO> getCsCollOwner() {
+    List<AppCollOwnerVO> csCollOwner = new ArrayList<>();
+    AppCollOwnerVO appCollOwner1 = new AppCollOwnerVO();
+    appCollOwner1.setNm("张三");
+    appCollOwner1.setMtCifIdTypCd("I");
+    appCollOwner1.setIdNo("110113198410126933");
+    appCollOwner1.setMobileNo("13512341234");
+    AppCollOwnerVO appCollOwner2 = new AppCollOwnerVO();
+    appCollOwner2.setNm("李四");
+    appCollOwner2.setMtCifIdTypCd("I");
+    appCollOwner2.setIdNo("110113198410126932");
+    appCollOwner2.setMobileNo("13512341232");
+    csCollOwner.add(appCollOwner1);
+    csCollOwner.add(appCollOwner2);
+    return csCollOwner;
+  }
+
+  /**
+   * @Description : 违章情况（测试数据）
+   * @return : MvPenaltyInfoVO
+   * @Creation Date : 2017年8月17日 下午8:07:00
+   * @Author : zhiqiang zhang
+   */
+  private MvPenaltyInfoVO getMvPenaltyInfo() {
+    MvPenaltyInfoVO mvPenaltyInfoVO = new MvPenaltyInfoVO();
+    mvPenaltyInfoVO.setUnclearedPenaltyPoints(5);
+    mvPenaltyInfoVO.setUnpaidPenalty(new BigDecimal("200"));
+    return mvPenaltyInfoVO;
+  }
+
+  /**
+   * @Description : 代收车船费（测试数据）
+   * @return : MvPaymentInfoVO
+   * @Creation Date : 2017年8月17日 下午8:06:43
+   * @Author : zhiqiang zhang
+   */
+  private MvPaymentInfoVO getMvPaymentInfo() {
+    MvPaymentInfoVO mvPaymentInfoVO = new MvPaymentInfoVO();
+    mvPaymentInfoVO.setCurYearUnpaid(new BigDecimal("200"));
+    mvPaymentInfoVO.setForfeit(new BigDecimal("200"));
+    mvPaymentInfoVO.setLastYearPaid(new BigDecimal("200"));
+    mvPaymentInfoVO.setFeeSum(new BigDecimal("600"));
+    Calendar calendar = Calendar.getInstance();
+    mvPaymentInfoVO.setPaymentYear(new Date(calendar.get(Calendar.YEAR)));
+    return mvPaymentInfoVO;
+  }
+
+  /**
+   * @Description : 交强险信息(测试数据)
+   * @return : MvInsuranceInfo
+   * @Creation Date : 2017年8月17日 下午8:06:24
+   * @Author : zhiqiang zhang
+   */
+  private MvInsuranceInfo getMvInsuranceInfo() {
+    MvInsuranceInfo insuranceInfo = new MvInsuranceInfo();
+    insuranceInfo.setDeathPayLimit(new BigDecimal("50000.01"));
+    insuranceInfo.setNoResponDeathPayLimit(new BigDecimal("50000.01"));
+    insuranceInfo.setNoResponMedicalPayLimit(new BigDecimal("50000.01"));
+    insuranceInfo.setNoResponPropertyLossPayLimit(new BigDecimal("50000.01"));
+    insuranceInfo.setPropertyLossPayLimit(new BigDecimal("50000.01"));
+    insuranceInfo.setMedicalPayLimit(new BigDecimal("50000.01"));
+    insuranceInfo.setInsuranceSum(new BigDecimal("50000.01"));
+    insuranceInfo.setRescueFundRate(new BigDecimal("52.22"));
+    insuranceInfo.setRescueFund(new BigDecimal("50000.01"));
+    insuranceInfo.setDtInsuranceStart(new Date());
+    insuranceInfo.setDtInsuranceEnd(new Date());
+    List<String> mtInsuranceResolutionCd = new ArrayList<>();
+    mtInsuranceResolutionCd.add("001");
+    mtInsuranceResolutionCd.add("002");
+    insuranceInfo.setMtInsuranceResolutionCd(mtInsuranceResolutionCd);
+    insuranceInfo.setHasInsurancePayHis("Y");
+    insuranceInfo.setEngineNo("123456789987654321");
+    insuranceInfo.setFrameNo("WFIE231231");
+    insuranceInfo.setPlateNo("JWF1231233");
+    insuranceInfo.setInsurancePaySum(new BigDecimal("50000.01"));
+    insuranceInfo.setAccidentIllegalRate(new BigDecimal("52"));
+    return insuranceInfo;
+  }
+
+  /**
+   * @Description : 车辆配置（测试数据）
+   * @return : MvConfInfoVO
+   * @Creation Date : 2017年8月17日 下午7:41:15
+   * @Author : zhiqiang zhang
+   */
+  private MvConfInfoVO getMvConfInfo() {
+    MvConfInfoVO confInfoVO = new MvConfInfoVO();
+    List<String> mtAirconditionTypCd = new ArrayList<>();
+    mtAirconditionTypCd.add("001");
+    mtAirconditionTypCd.add("002");
+    confInfoVO.setMtAirconditionTypCd(mtAirconditionTypCd);
+    List<String> mtCommonConfTypCd = new ArrayList<>();
+    mtCommonConfTypCd.add("001");
+    mtCommonConfTypCd.add("002");
+    confInfoVO.setMtCommonConfTypCd(mtCommonConfTypCd);
+    List<String> mtDriverEleTypCd = new ArrayList<>();
+    mtDriverEleTypCd.add("001");
+    mtDriverEleTypCd.add("002");
+    confInfoVO.setMtDriverEleTypCd(mtDriverEleTypCd);
+    List<String> mtGearboxTypCd = new ArrayList<>();
+    mtGearboxTypCd.add("001");
+    mtGearboxTypCd.add("002");
+    confInfoVO.setMtGearboxTypCd(mtGearboxTypCd);
+    List<String> mtParkingRadarTypCd = new ArrayList<>();
+    mtParkingRadarTypCd.add("001");
+    mtParkingRadarTypCd.add("002");
+    confInfoVO.setMtParkingRadarTypCd(mtParkingRadarTypCd);
+    List<String> mtSeatHeatTypCd = new ArrayList<>();
+    mtSeatHeatTypCd.add("001");
+    mtSeatHeatTypCd.add("002");
+    confInfoVO.setMtSeatHeatTypCd(mtSeatHeatTypCd);
+    return confInfoVO;
+  }
+
+  /**
+   * @Description : 机动车基本信息（测试数据）
+   * @return : MvBasicInfoVO
+   * @Creation Date : 2017年8月17日 下午7:39:17
+   * @Author : zhiqiang zhang
+   */
+  private MvBasicInfoVO getMvBasicInfo() {
+    MvBasicInfoVO basicInfoVO = new MvBasicInfoVO();
+    basicInfoVO.setDtFirstRecord(new Date());
+    basicInfoVO.setVehicleType("汽车");
+    basicInfoVO.setBrand("奥迪");
+    basicInfoVO.setApprovedMass(new BigDecimal("1000"));
+    basicInfoVO.setApprovedPassenger(6);
+    basicInfoVO.setColor("红");
+    basicInfoVO.setDrivenDistance(new BigDecimal("1000"));
+    basicInfoVO.setEngineCapacity(new BigDecimal("2.0"));
+    basicInfoVO.setIsMadeInChina("N");
+    basicInfoVO.setManufactured(new Date());
+    basicInfoVO.setModel("轿车");
+    List<String> mtFuelCd = new ArrayList<>();
+    mtFuelCd.add("001");
+    mtFuelCd.add("002");
+    mtFuelCd.add("003");
+    basicInfoVO.setMtFuelCd(mtFuelCd);
+    basicInfoVO.setPower(new BigDecimal("232.0"));
+    basicInfoVO.setPriceMax(new BigDecimal("500000.01"));
+    basicInfoVO.setPriceMin(new BigDecimal("20000.01"));
+    basicInfoVO.setSeries("美系车");
+    return basicInfoVO;
   }
 }
--- src/main/java/com/ljt/openapi/demo/demos/AddCollPrDemo.java
+++ src/main/java/com/ljt/openapi/demo/demos/AddCollPrDemo.java
@@ -56,139 +56,6 @@ public class AddCollPrDemo {
    */
   private String appSecret = "";
 
-  /**
-   * @Description : 新增房产担保品请求参数示例
-   * @return : String
-   * @Creation Date : 2017年8月17日 下午1:57:01
-   * @Author : zhiqiang zhang
-   */
-  public String collPrParams() {
-    AppCollPrVO appCollPrVO = new AppCollPrVO();
-    appCollPrVO.setAppId("0092728480d24f5d87bf63639b5cfe1c");
-    appCollPrVO.setMtCollStyleCd("DY");
-    appCollPrVO.setMtCollCatCd("PR01");
-    appCollPrVO.setMtCollCd("DY0101001");
-    appCollPrVO.setCollValue(new BigDecimal("123456.78"));
-    appCollPrVO.setDtPurchased(new Date());
-    appCollPrVO.setMortgageInfo("抵押摘要");
-    appCollPrVO.setPurchasedPrc(new BigDecimal("500000.78"));
-
-    /*
-     * 房产基本信息
-     */
-    PrBasicInfoVO prBasicInfo = new PrBasicInfoVO();
-    prBasicInfo.setBuildingNm("建筑物名称");
-    prBasicInfo.setMtCollShareCd("001");
-    prBasicInfo.setAddrLine("美国");
-    prBasicInfo.setContractNo("234234324234");
-    prBasicInfo.setPropertyNo("");
-    prBasicInfo.setDtRecord(new Date());
-    prBasicInfo.setAge(22);
-    prBasicInfo.setDtFinished(new Date());
-    prBasicInfo.setHasElevator("Y");
-    prBasicInfo.setMonthPropertyFee(new BigDecimal("1555.22"));
-    prBasicInfo.setMtPrtyFacingCd("006");
-    prBasicInfo.setMtPrtyUseCd("005");
-    prBasicInfo.setOtherPrtyUseDscp("工业用地");
-    prBasicInfo.setPropertyCompany("美国物业公司");
-    appCollPrVO.setPrBasicInfo(prBasicInfo);
-
-    /*
-     * 房屋构造情况
-     */
-    PrConsttInfoVO prConsttInfo = new PrConsttInfoVO();
-    prConsttInfo.setMtConsttFlrCd("001");
-    prConsttInfo.setMtConsttRoofCd("001");
-    prConsttInfo.setMtConsttWallCd("001");
-    prConsttInfo.setOtherConsttFlrDscp("地面构造");
-    prConsttInfo.setOtherConsttRoofDscp("屋顶构造");
-    prConsttInfo.setOtherConsttWallDscp("墙壁构造");
-    prConsttInfo.setRemark("建筑物说明");
-    appCollPrVO.setPrConsttInfo(prConsttInfo);
-
-    /*
-     * 土地状况
-     */
-    PrLandInfoVO prLandInfo = new PrLandInfoVO();
-    prLandInfo.setDtLandEnd(new Date());
-    prLandInfo.setDtLandStarted(new Date());
-    prLandInfo.setLandNo("KL92819928177281");
-    prLandInfo.setLandUseLife(88);
-    prLandInfo.setMtLandAcquireMtdCd("001");
-    prLandInfo.setOtherLandAcquireMtdDscp("土地使用权取得方式");
-    appCollPrVO.setPrLandInfo(prLandInfo);
-
-    /*
-     * 租赁信息
-     */
-    PrLeaseInfoVO prLeaseInfoVO = new PrLeaseInfoVO();
-    prLeaseInfoVO.setDtEffective(new Date());
-    prLeaseInfoVO.setIsLease("Y");
-    prLeaseInfoVO.setLesseeNm("张三");
-    appCollPrVO.setPrLeaseInfo(prLeaseInfoVO);
-
-    /*
-     * 房屋登记情况
-     */
-    PrRecordInfoVO prRecordInfoVO = new PrRecordInfoVO();
-    prRecordInfoVO.setBuildingNo("5");
-    prRecordInfoVO.setBuiltUpArea(new BigDecimal("123.5"));
-    prRecordInfoVO.setFloorNo(12);
-    prRecordInfoVO.setHousingBalconyArea(new BigDecimal("123.5"));
-    prRecordInfoVO.setHousingCoveredArea(new BigDecimal("123.5"));
-    prRecordInfoVO.setHousingShareArea(new BigDecimal("123.5"));
-    prRecordInfoVO.setHousingUsefulArea(new BigDecimal("123.5"));
-    prRecordInfoVO.setRoomNo("122");
-    prRecordInfoVO.setRoomNum(12);
-    prRecordInfoVO.setStructure("砖混结构");
-    prRecordInfoVO.setTotalFloorNum(15);
-    appCollPrVO.setPrRecordInfo(prRecordInfoVO);
-
-    /*
-     * 苏有着信息（个人）
-     */
-    List<AppCollOwnerVO> csCollOwner = new ArrayList<>();
-    AppCollOwnerVO appCollOwner1 = new AppCollOwnerVO();
-    appCollOwner1.setNm("张三");
-    appCollOwner1.setMtCifIdTypCd("I");
-    appCollOwner1.setIdNo("110113198410126933");
-    appCollOwner1.setMobileNo("13512341234");
-    AppCollOwnerVO appCollOwner2 = new AppCollOwnerVO();
-    appCollOwner2.setNm("李四");
-    appCollOwner2.setMtCifIdTypCd("I");
-    appCollOwner2.setIdNo("110113198410126932");
-    appCollOwner2.setMobileNo("13512341232");
-    csCollOwner.add(appCollOwner1);
-    csCollOwner.add(appCollOwner2);
-    appCollPrVO.setCsCollOwner(csCollOwner);
-    /*
-     * 苏有着信息（企业）
-     */
-    List<AppCollOwnerVO> cpCollOwner = new ArrayList<>();
-    AppCollOwnerVO appCollOwner3 = new AppCollOwnerVO();
-    appCollOwner3.setNm("北京某某公司");
-    appCollOwner3.setIdNo("110113198410126911");
-    appCollOwner3.setIsComb("Y");
-    AppCollOwnerVO appCollOwner4 = new AppCollOwnerVO();
-    appCollOwner4.setNm("上海某某公司");
-    appCollOwner4.setIdNo("110113198410126922");
-    appCollOwner4.setIsComb("N");
-    cpCollOwner.add(appCollOwner3);
-    cpCollOwner.add(appCollOwner4);
-    appCollPrVO.setCsCollOwner(cpCollOwner);
-    /*
-     * 外部评估信息
-     */
-    List<AppCollEvaluateVO> appCollEvaluate = new ArrayList<>();
-    AppCollEvaluateVO appCollEvaluateVO = new AppCollEvaluateVO();
-    appCollEvaluateVO.setEvaluateValue(new BigDecimal("123456.78"));
-    appCollEvaluateVO.setDtEvaluated(new Date());
-    appCollEvaluateVO.setAppraiser("李四");
-    appCollEvaluate.add(appCollEvaluateVO);
-    appCollPrVO.setAppCollEvaluate(appCollEvaluate);
-
-    return JSONObject.toJSONString(appCollPrVO, SerializerFeature.WriteDateUseDateFormat);
-  }
 
   /**
    * @Description : 财报上传demo
@@ -224,4 +91,197 @@ public class AddCollPrDemo {
     request.setStringBody(requestBody);
     Client.execute(request);
   }
+
+  /**
+   * @Description : 新增房产担保品请求参数示例
+   * @return : String
+   * @Creation Date : 2017年8月17日 下午1:57:01
+   * @Author : zhiqiang zhang
+   */
+  public String collPrParams() {
+    AppCollPrVO appCollPrVO = new AppCollPrVO();
+    appCollPrVO.setAppId("0092728480d24f5d87bf63639b5cfe1c");
+    appCollPrVO.setMtCollStyleCd("DY");
+    appCollPrVO.setMtCollCatCd("PR01");
+    appCollPrVO.setMtCollCd("DY0101001");
+    appCollPrVO.setCollValue(new BigDecimal("123456.78"));
+    appCollPrVO.setDtPurchased(new Date());
+    appCollPrVO.setMortgageInfo("抵押摘要");
+    appCollPrVO.setPurchasedPrc(new BigDecimal("500000.78"));
+    // 房产基本信息
+    appCollPrVO.setPrBasicInfo(getPrBasicInfo());
+    // 房屋构造情况
+    appCollPrVO.setPrConsttInfo(getPrConsttInfo());
+    // 土地状况
+    appCollPrVO.setPrLandInfo(getPrLandInfo());
+    // 租赁信息
+    appCollPrVO.setPrLeaseInfo(getPrLeaseInfo());
+    // 房屋登记情况
+    appCollPrVO.setPrRecordInfo(getPrRecordInfo());
+    // 所有者信息（个人）
+    appCollPrVO.setCsCollOwner(getCsCollOwner());
+    // 所有者信息（企业）
+    appCollPrVO.setCsCollOwner(getCpCollOwner());
+    // 外部评估信息
+    appCollPrVO.setAppCollEvaluate(getAppCollEvaluate());
+    return JSONObject.toJSONString(appCollPrVO, SerializerFeature.WriteDateUseDateFormat);
+  }
+
+  /**
+   * @Description : 外部评估信息（测试数据）
+   * @return : List<AppCollEvaluateVO>
+   * @Creation Date : 2017年8月17日 下午7:35:45
+   * @Author : zhiqiang zhang
+   */
+  private List<AppCollEvaluateVO> getAppCollEvaluate() {
+    List<AppCollEvaluateVO> appCollEvaluate = new ArrayList<>();
+    AppCollEvaluateVO appCollEvaluateVO = new AppCollEvaluateVO();
+    appCollEvaluateVO.setEvaluateValue(new BigDecimal("123456.78"));
+    appCollEvaluateVO.setDtEvaluated(new Date());
+    appCollEvaluateVO.setAppraiser("李四");
+    appCollEvaluate.add(appCollEvaluateVO);
+    return appCollEvaluate;
+  }
+
+  /**
+   * @Description : 所有者信息（企业）（测试数据）
+   * @return : List<AppCollOwnerVO>
+   * @Creation Date : 2017年8月17日 下午7:34:41
+   * @Author : zhiqiang zhang
+   */
+  private List<AppCollOwnerVO> getCpCollOwner() {
+    List<AppCollOwnerVO> cpCollOwner = new ArrayList<>();
+    AppCollOwnerVO appCollOwner3 = new AppCollOwnerVO();
+    appCollOwner3.setNm("北京某某公司");
+    appCollOwner3.setIdNo("110113198410126911");
+    appCollOwner3.setIsComb("Y");
+    AppCollOwnerVO appCollOwner4 = new AppCollOwnerVO();
+    appCollOwner4.setNm("上海某某公司");
+    appCollOwner4.setIdNo("110113198410126922");
+    appCollOwner4.setIsComb("N");
+    cpCollOwner.add(appCollOwner3);
+    cpCollOwner.add(appCollOwner4);
+    return cpCollOwner;
+  }
+
+  /**
+   * @Description : 所有者信息（个人）（测试数据）
+   * @return : List<AppCollOwnerVO>
+   * @Creation Date : 2017年8月17日 下午7:33:37
+   * @Author : zhiqiang zhang
+   */
+  private List<AppCollOwnerVO> getCsCollOwner() {
+    List<AppCollOwnerVO> csCollOwner = new ArrayList<>();
+    AppCollOwnerVO appCollOwner1 = new AppCollOwnerVO();
+    appCollOwner1.setNm("张三");
+    appCollOwner1.setMtCifIdTypCd("I");
+    appCollOwner1.setIdNo("110113198410126933");
+    appCollOwner1.setMobileNo("13512341234");
+    AppCollOwnerVO appCollOwner2 = new AppCollOwnerVO();
+    appCollOwner2.setNm("李四");
+    appCollOwner2.setMtCifIdTypCd("I");
+    appCollOwner2.setIdNo("110113198410126932");
+    appCollOwner2.setMobileNo("13512341232");
+    csCollOwner.add(appCollOwner1);
+    csCollOwner.add(appCollOwner2);
+    return csCollOwner;
+  }
+
+  /**
+   * @Description : 房屋登记情况（测试数据）
+   * @return : PrRecordInfoVO
+   * @Creation Date : 2017年8月17日 下午7:32:12
+   * @Author : zhiqiang zhang
+   */
+  private PrRecordInfoVO getPrRecordInfo() {
+    PrRecordInfoVO prRecordInfoVO = new PrRecordInfoVO();
+    prRecordInfoVO.setBuildingNo("5");
+    prRecordInfoVO.setBuiltUpArea(new BigDecimal("123.5"));
+    prRecordInfoVO.setFloorNo(12);
+    prRecordInfoVO.setHousingBalconyArea(new BigDecimal("123.5"));
+    prRecordInfoVO.setHousingCoveredArea(new BigDecimal("123.5"));
+    prRecordInfoVO.setHousingShareArea(new BigDecimal("123.5"));
+    prRecordInfoVO.setHousingUsefulArea(new BigDecimal("123.5"));
+    prRecordInfoVO.setRoomNo("122");
+    prRecordInfoVO.setRoomNum(12);
+    prRecordInfoVO.setStructure("砖混结构");
+    prRecordInfoVO.setTotalFloorNum(15);
+    return prRecordInfoVO;
+  }
+
+  /**
+   * @Description : 租赁信息(测试数据)
+   * @return : PrLeaseInfoVO
+   * @Creation Date : 2017年8月17日 下午7:31:21
+   * @Author : zhiqiang zhang
+   */
+  private PrLeaseInfoVO getPrLeaseInfo() {
+    PrLeaseInfoVO prLeaseInfoVO = new PrLeaseInfoVO();
+    prLeaseInfoVO.setDtEffective(new Date());
+    prLeaseInfoVO.setIsLease("Y");
+    prLeaseInfoVO.setLesseeNm("张三");
+    return prLeaseInfoVO;
+  }
+
+  /**
+   * @Description : 土地状况（测试数据）
+   * @return : PrLandInfoVO
+   * @Creation Date : 2017年8月17日 下午7:30:26
+   * @Author : zhiqiang zhang
+   */
+  private PrLandInfoVO getPrLandInfo() {
+    PrLandInfoVO prLandInfo = new PrLandInfoVO();
+    prLandInfo.setDtLandEnd(new Date());
+    prLandInfo.setDtLandStarted(new Date());
+    prLandInfo.setLandNo("KL92819928177281");
+    prLandInfo.setLandUseLife(88);
+    prLandInfo.setMtLandAcquireMtdCd("001");
+    prLandInfo.setOtherLandAcquireMtdDscp("土地使用权取得方式");
+    return prLandInfo;
+  }
+
+  /**
+   * @Description : 房屋构造情况(测试数据)
+   * @return : PrConsttInfoVO
+   * @Creation Date : 2017年8月17日 下午7:29:21
+   * @Author : zhiqiang zhang
+   */
+  private PrConsttInfoVO getPrConsttInfo() {
+    PrConsttInfoVO prConsttInfo = new PrConsttInfoVO();
+    prConsttInfo.setMtConsttFlrCd("001");
+    prConsttInfo.setMtConsttRoofCd("001");
+    prConsttInfo.setMtConsttWallCd("001");
+    prConsttInfo.setOtherConsttFlrDscp("地面构造");
+    prConsttInfo.setOtherConsttRoofDscp("屋顶构造");
+    prConsttInfo.setOtherConsttWallDscp("墙壁构造");
+    prConsttInfo.setRemark("建筑物说明");
+    return prConsttInfo;
+  }
+
+  /**
+   * @Description : 房产基本信息（测试数据）
+   * @return : PrBasicInfoVO
+   * @Creation Date : 2017年8月17日 下午7:27:36
+   * @Author : zhiqiang zhang
+   */
+  private PrBasicInfoVO getPrBasicInfo() {
+    PrBasicInfoVO prBasicInfo = new PrBasicInfoVO();
+    prBasicInfo.setBuildingNm("建筑物名称");
+    prBasicInfo.setMtCollShareCd("001");
+    prBasicInfo.setAddrLine("美国");
+    prBasicInfo.setContractNo("234234324234");
+    prBasicInfo.setPropertyNo("");
+    prBasicInfo.setDtRecord(new Date());
+    prBasicInfo.setAge(22);
+    prBasicInfo.setDtFinished(new Date());
+    prBasicInfo.setHasElevator("Y");
+    prBasicInfo.setMonthPropertyFee(new BigDecimal("1555.22"));
+    prBasicInfo.setMtPrtyFacingCd("006");
+    prBasicInfo.setMtPrtyUseCd("005");
+    prBasicInfo.setOtherPrtyUseDscp("工业用地");
+    prBasicInfo.setPropertyCompany("美国物业公司");
+    return prBasicInfo;
+  }
+
+
 }
