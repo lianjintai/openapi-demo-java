6dd391bb5653a858c2c24500cc63daa7b045159c
Zhouzb <zhouzb@lianjintai.com>
2018-11-07 02:05:20 +0800

    fix：个贷字段配置demo
      fix：个贷字段配置demo

--- src/main/java/com/ljt/openapi/demo/demos/CreditAppCsFacConfDemo.java
+++ src/main/java/com/ljt/openapi/demo/demos/CreditAppCsFacConfDemo.java
@@ -1,15 +1,18 @@
 package com.ljt.openapi.demo.demos;
 
+import com.alibaba.fastjson.JSONObject;
 import com.ljt.openapi.demo.Client;
 import com.ljt.openapi.demo.Request;
 import com.ljt.openapi.demo.Response;
 import com.ljt.openapi.demo.constant.Constants;
+import com.ljt.openapi.demo.constant.ContentType;
 import com.ljt.openapi.demo.constant.HttpHeader;
 import com.ljt.openapi.demo.constant.HttpSchema;
 import com.ljt.openapi.demo.constant.SystemHeader;
 import com.ljt.openapi.demo.enums.ApiHost;
 import com.ljt.openapi.demo.enums.Method;
 import com.ljt.openapi.demo.util.AESUtil;
+import com.ljt.openapi.demo.util.MessageDigestUtil;
 import com.ljt.openapi.demo.util.PropertiesUtils;
 import java.util.HashMap;
 import java.util.Map;
@@ -44,14 +47,18 @@ public class CreditAppCsFacConfDemo {
 
   @Test
   public void creditAppCsFacConfTest() throws Exception {
-
-    String url = "/v2/cif/apps/mt_fac_cd/{mt_fac_cd}/fields";
+    // 构建请求体
+    Map<String, String> map = new HashMap<>();
+    map.put("mt_fac_cd", "P104835");
+    String requestBody = JSONObject.toJSONString(map);
+    String method="loan_app:cs_fac:config";
     Request request = new Request();
-    request.setMethod(Method.GET);
+    request.setMethod(Method.POST_STRING);
     /**
      * 测试环境，生产环境需要修改为 HttpSchema.HTTPS OFFICIAL_API_HOST
      */
-    request.setHost(HttpSchema.HTTPS + ApiHost.DEV_API_HOST.getHost()+url);
+    request.setHost(HttpSchema.HTTPS + ApiHost.DEV_API_HOST.getHost());
+    request.setPath("v1/gateway"+method);
     request.setAppKey(appKey);
     request.setAppSecret(appSecret);
     request.setTimeout(Constants.DEFAULT_TIMEOUT);
@@ -59,7 +66,13 @@ public class CreditAppCsFacConfDemo {
     headers.put(SystemHeader.X_CA_NONCE, UUID.randomUUID().toString());
     // （必填）根据期望的Response内容类型设置
     headers.put(HttpHeader.HTTP_HEADER_ACCEPT, "application/json");
+    // Body MD5,服务端会校验Body内容是否被篡改,建议Body非Form表单时添加此Header
+    requestBody = AESUtil.encrypt(key, requestBody);
+    headers.put(HttpHeader.HTTP_HEADER_CONTENT_MD5, MessageDigestUtil.base64AndMD5(requestBody));
+    // （POST/PUT请求必选）请求Body内容格式
+    headers.put(HttpHeader.HTTP_HEADER_CONTENT_TYPE, ContentType.CONTENT_TYPE_TEXT);
     request.setHeaders(headers);
+    request.setStringBody(requestBody);
     Response response = Client.execute(request);
     if (response.getStatusCode() == 200) {
       System.out.println("decrypt response：" + AESUtil.decrypt(response.getBody(), key));
